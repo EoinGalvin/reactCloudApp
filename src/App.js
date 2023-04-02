2fc9117ded486ce2e6acf7a10f2799077cf3a092
EoinGalvin <eoin.j.galvin@students.ittralee.ie>
2023-04-02 22:53:35 +0100

    cognito added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import Title from "./Title"
-import LearningMaterials from "./LearningMaterial";
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './Configuration';
+import Title from "./Title";
 import SearchQuery from "./SearchQuery";
+Amplify.configure(awsExports);
 
 function App() {
-  return <div>
-  <Title></Title>
-  <SearchQuery></SearchQuery>
-  </div>
+  return (
+    <div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+            <Title></Title>
+            <SearchQuery></SearchQuery>
+          </main>
+        )}
+      </Authenticator>
+    </div>)
 }
 
 export default App;
---
+++ src/Configuration.js
@@ -0,0 +1,8 @@
+const awsExports = {
+    Auth: {
+        'userPoolId': 'us-east-1_VKb8A8fOk',
+        'userPoolWebClientId': '18ffmj78d1bk1c3h0p39s7us10',
+    }
+}
+
+export default awsExports;
